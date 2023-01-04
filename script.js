f4e489c48740b8538791b236b9a5190603a29fbb
Aaron Lapworth <aaronlapworth@hotmail.co.uk>
2023-01-04 19:47:53 +0000

    adding return for best practive

--- script.js
+++ script.js
@@ -182,6 +182,7 @@ function writePassword() {
     var passwordText = document.querySelector('#password');
 
     passwordText.value = password;
+    return;
 }
 
 // Add event listener to generate button
